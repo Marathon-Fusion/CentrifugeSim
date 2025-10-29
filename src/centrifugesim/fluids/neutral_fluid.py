4eb15b33d878f96a6f9d57149a7f0e59364473c9
mdac95 <acciarri@umich.edu>
2025-10-29 11:03:20 -0700

    fixed bugs in particle kernels, added test functions fluid equations

--- src/centrifugesim/fluids/neutral_fluid.py
+++ src/centrifugesim/fluids/neutral_fluid.py
@@ -7,7 +7,9 @@ from centrifugesim import constants
 class NeutralFluidContainer:
     """
     """
-    def __init__(self, geom:Geometry, nn_floor, mass):
+    def __init__(self, geom:Geometry, nn_floor, mass, name, Tn0=0.0):
+
+        self.name = name
 
         self.Nr = geom.Nr
         self.Nz = geom.Nz
@@ -15,10 +17,27 @@ class NeutralFluidContainer:
         self.nn_floor = nn_floor
         self.mass = mass
 
-        self.nn_grid = np.zeros((self.Nr, self.Nz))
+        self.nn_grid = np.zeros((self.Nr, self.Nz)).astype(np.float64)
 
-        self.un_r_grid = np.zeros((self.Nr, self.Nz))
-        self.un_theta_grid = np.zeros((self.Nr, self.Nz))
-        self.un_z_grid = np.zeros((self.Nr, self.Nz))
+        self.un_r_grid = np.zeros((self.Nr, self.Nz)).astype(np.float64)
+        self.un_theta_grid = np.zeros((self.Nr, self.Nz)).astype(np.float64)
+        self.un_z_grid = np.zeros((self.Nr, self.Nz)).astype(np.float64)
         
-        self.T_n_grid = np.zeros((self.Nr, self.Nz))
+        self.T_n_grid = Tn0*np.ones((self.Nr, self.Nz)).astype(np.float64)
+
+        print(self.name + " initialized")
+
+    def update_utheta(self, geom, ui_theta_grid, nu_in_grid, ni_grid, mi, dt):
+        """
+        Just to test before merging with compressible Navier Stokes equations.
+        """
+        dtnu_max = nu_in_grid[geom.mask==1].max()*dt
+        if(dtnu_max>0.1):
+            print("dt*nu_in.max() > 0.1 !", dtnu_max)
+
+        un_theta_new = neutral_fluid_kernels_numba.test_update_u_theta(geom.mask,
+                        ni_grid*mi, self.nn_grid*self.mass,
+                        ui_theta_grid, self.un_theta,
+                        nu_in_grid, self.nn_floor*self.mass, dt)
+
+        self.un_theta[geom.mask==1] = np.copy(un_theta_new[geom.mask==1])
